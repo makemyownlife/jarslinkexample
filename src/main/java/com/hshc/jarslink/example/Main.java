0da962dbd23beb0be14dd828fe43f0eceb8d66a5
zhangyong <zhangyong7120180@163.com>
2018-08-12 16:50:08 +0800

    添加 spring modules 例子

--- src/main/java/com/hshc/jarslink/example/Main.java
+++ src/main/java/com/hshc/jarslink/example/Main.java
@@ -1,12 +1,15 @@
 package com.hshc.jarslink.example;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by zhangyong on 2018/8/12.
  */
 public class Main {
 
     public static void main(String[] args) {
-        
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("classpath:spring-app.xml");
+        classPathXmlApplicationContext.start();
     }
 
 }
