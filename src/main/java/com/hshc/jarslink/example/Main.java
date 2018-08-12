5c516792711ceafc511a496cc41967c9d0557749
zhangyong <zhangyong7120180@163.com>
2018-08-12 15:09:34 +0800

    jars link 例子

---
+++ src/main/java/com/hshc/jarslink/example/Main.java
@@ -0,0 +1,12 @@
+package com.hshc.jarslink.example;
+
+/**
+ * Created by zhangyong on 2018/8/12.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(12312);
+    }
+
+}
